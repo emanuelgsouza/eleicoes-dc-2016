5dbfd8775e9129f6679418cd5f8637b9fa56ac9c
Emanuel G <emanuelgdesouza@gmail.com>
2018-11-15 18:39:23 -0200

    update setup

--- setup.sh
+++ setup.sh
@@ -6,8 +6,12 @@ cd analysis
 # convert ipynb file to html file
 $HOME/anaconda3/bin/jupyter nbconvert --to html eleicoes-dc-analysis.ipynb
 
+$HOME/anaconda3/bin/jupyter nbconvert --to html geographic-analysis.ipynb
+
 # move html file to docs folder
 mv eleicoes-dc-analysis.html ../docs
+
+mv geographic-analysis.html ../docs
 
 # enter in docs folder
 cd ../docs
@@ -15,11 +19,15 @@ cd ../docs
 # rename file to entry point
 mv eleicoes-dc-analysis.html index.html
 
+mv geographic-analysis.html geographic.html
+
 # come in back to root folder
 cd ..
 
 # add index.html to staging (GIT)
 git add docs/index.html
+
+git add docs/geographic.html
 
 # commit
 git commit -m"update docs"
